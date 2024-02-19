f566556c9e16099d1aa4e97d15bd7dba301ca551
TwilDust <thecrecker666@gmail.com>
2024-02-19 12:18:28 +0300

    Task output with use for in

--- task_5/main.js
+++ task_5/main.js
@@ -6,3 +6,23 @@ let task = {
     completeonTime: 20, // in minutes
     maxCompleteonTime: 60,
 };
+
+for(item in task)
+{
+    if (item === "complexity")
+    {
+        console.log(`Сложность задания: ${task[item]}`);
+        continue;
+    }
+    else if (item === "completeonTime")
+    {
+        console.log(`Предпологаемое время выполнения: ${task[item]}`);
+        continue;
+    }
+    else if(item === "maxCompleteonTime")
+    {
+        console.log(`Максимально допустипое время выполнения: ${task[item]}`);
+        continue;
+    }
+    console.log(task[item]);
+}
