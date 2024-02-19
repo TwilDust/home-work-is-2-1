39e440d88d21ddaecd1a93f807ecdeeb4699aaea
TwilDust <thecrecker666@gmail.com>
2024-02-19 12:35:20 +0300

    Task complexity output

--- task_5/main.js
+++ task_5/main.js
@@ -9,15 +9,35 @@ let task = {
 
 for(item in task)
 {
-    if (item === "completeonTime")
+    if (item === "complexity")
+    {
+        console.log(`Сложность задания: ${task[item]}`);
+        continue;
+    }
+    else if (item === "completeonTime")
     {
         console.log(`Предпологаемое время выполнения: ${task[item]}`);
         continue;
     }
     else if(item === "maxCompleteonTime")
     {
-        console.log(`Максимально допустипое время выполнения: ${task[item]}`);
+        console.log(`Максимально допустипое время выпол: ${task[item]}`);
         continue;
     }
     console.log(task[item]);
+}
+
+const taskComplexity = task["complexity"];
+
+if (taskComplexity === 1)
+{
+    console.log("Это не сложная задача!");
+}
+else if (taskComplexity === 2)
+{
+    console.log("Эта задача средней сложности!");
+}
+else if (taskComplexity === 3)
+{
+    console.log("Это сложная задача!");
 }
